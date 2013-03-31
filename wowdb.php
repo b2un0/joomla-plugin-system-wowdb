774dbd7ef8b798397c64d3870ab6cdb15bb7cc67
Branko Wilhelm <branko.wilhelm@gmail.com>
2013-03-31 23:43:22 +0000

    erste funktionierende version

---
+++ wowdb.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * wowdb
+ *
+ * @author     Branko Wilhelm <bw@z-index.net>
+ * @link       http://www.z-index.net
+ * @copyright  (c) 2013 Branko Wilhelm
+ * @package    plg_wowdb
+ * @license    GNU General Public License v3
+ * @version    $Id$
+ */
+ 
+// no direct access
+defined('_JEXEC') or die;
+
+class plgSystemWoWDB extends JPlugin {
+
+	public function onBeforeRender() {
+		if($this->params->get('jquery')) {
+			JFactory::getDocument()->addScript('//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js');
+		}
+		
+		JFactory::getDocument()->addScript('//static-azeroth.cursecdn.com/current/js/syndication/tt.js');
+    }
+}
