aa21622933f4a6ec02af051c7e98a352f3dcde02
Branko Wilhelm <branko.wilhelm@gmail.com>
2013-04-01 12:55:53 +0000

    jQuery noConflict added

--- wowdb.php
+++ wowdb.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * wowdb
+ * WoWDB
  *
  * @author     Branko Wilhelm <bw@z-index.net>
  * @link       http://www.z-index.net
@@ -17,10 +17,23 @@ defined('_JEXEC') or die;
 class plgSystemWoWDB extends JPlugin {
 
 	public function onBeforeRender() {
-		if($this->params->get('jquery')) {
-			JFactory::getDocument()->addScript('//ajax.googleapis.com/ajax/libs/jquery/' . $this->params->get('jquery_branch') . '/jquery.min.js');
+		if (JFactory::getApplication()->isAdmin()) {
+			return;
 		}
 		
-		JFactory::getDocument()->addScript('//static-azeroth.cursecdn.com/current/js/syndication/tt.js');
+		if($this->params->get('jquery', 1)) {
+			$jquery = '//ajax.googleapis.com/ajax/libs/jquery/' . $this->params->get('jquery_branch') . '/jquery.min.js';
+			
+			if($this->params->get('jquery_noconflict', 1)) {
+				$js[] = '<script src="' . $jquery . '" type="text/javascript"></script>';
+				$js[] = '<script>$.noConflict()</script>';
+				$js[] = '<script src="//static-azeroth.cursecdn.com/current/js/syndication/tt.js" type="text/javascript"></script>';
+				
+				JFactory::getDocument()->addCustomTag(implode(PHP_EOL, $js));
+			}else{
+				JFactory::getDocument()->addScript($jquery);
+				JFactory::getDocument()->addScript('//static-azeroth.cursecdn.com/current/js/syndication/tt.js');
+			}
+		}
     }
 }
