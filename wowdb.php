8145344ade64e487cb8882558fc173dca6a4e569
b2un0 <branko.wilhelm@gmail.com>
2014-01-17 19:05:48 +0100

    ide switch

--- wowdb.php
+++ wowdb.php
@@ -3,50 +3,51 @@
 /**
  * @author     Branko Wilhelm <branko.wilhelm@gmail.com>
  * @link       http://www.z-index.net
- * @copyright  (c) 2013 Branko Wilhelm
+ * @copyright  (c) 2013 - 2014 Branko Wilhelm
  * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
- 
+
 defined('_JEXEC') or die;
 
-class plgSystemWowdb extends JPlugin {
-	
-	private $js = '//static-azeroth.cursecdn.com/current/js/syndication/tt.js';
-	
-	private $advanced = 'var WowDbSettings={AdvancedTooltips:true};';
-	
-	public function onBeforeRender() {
-		if (JFactory::getApplication()->isAdmin()) {
-			return;
-		}
-		
-		$document = JFactory::getDocument();
-		
-		if($this->params->get('advanced')) {
-			$document->addScriptDeclaration($this->advanced);
-		}
-		
-		if(JVersion::isCompatible('3')) {
-			JHtml::_('jquery.framework');
-			$document->addScript($this->js, 'text/javascript', false, true);
-			return; 
-		}
-		
-		if($this->params->get('jquery', 1)) {
-			$jquery = '//ajax.googleapis.com/ajax/libs/jquery/' . $this->params->get('jquery_branch') . '/jquery.min.js';
-			
-			if($this->params->get('jquery_noconflict', 1)) {
-				$jq[] = '<script src="' . $jquery . '" type="text/javascript"></script>';
-				$jq[] = '<script type="text/javascript">$.noConflict()</script>';
-				$jq[] = '<script src="' . $this->js . '" type="text/javascript"></script>';
+class plgSystemWowdb extends JPlugin
+{
 
-				$document->addCustomTag(implode(PHP_EOL, $jq));
-			}else{
-				$document->addScript($jquery);
-				$document->addScript($this->js, 'text/javascript', false, true);
-			}
-		}else{
-			$document->addScript($this->js, 'text/javascript', false, true);
-		}
+    private $js = '//static-azeroth.cursecdn.com/current/js/syndication/tt.js';
+    private $advanced = 'var WowDbSettings={AdvancedTooltips:true};';
+
+    public function onBeforeRender()
+    {
+        if (JFactory::getApplication()->isAdmin()) {
+            return;
+        }
+
+        $document = JFactory::getDocument();
+
+        if ($this->params->get('advanced')) {
+            $document->addScriptDeclaration($this->advanced);
+        }
+
+        if (JVersion::isCompatible('3')) {
+            JHtml::_('jquery.framework');
+            $document->addScript($this->js, 'text/javascript', false, true);
+            return;
+        }
+
+        if ($this->params->get('jquery', 1)) {
+            $jquery = '//ajax.googleapis.com/ajax/libs/jquery/' . $this->params->get('jquery_branch') . '/jquery.min.js';
+
+            if ($this->params->get('jquery_noconflict', 1)) {
+                $jq[] = '<script src="' . $jquery . '"></script>';
+                $jq[] = '<script type="text/javascript">$.noConflict()</script>';
+                $jq[] = '<script src="' . $this->js . '"></script>';
+
+                $document->addCustomTag(implode(PHP_EOL, $jq));
+            } else {
+                $document->addScript($jquery);
+                $document->addScript($this->js, 'text/javascript', false, true);
+            }
+        } else {
+            $document->addScript($this->js, 'text/javascript', false, true);
+        }
     }
 }
